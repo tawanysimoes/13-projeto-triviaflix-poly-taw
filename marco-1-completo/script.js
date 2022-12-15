1dd92e1034409abc26cab19fc265887b41fa6171
tawanysimoes <tawanyjor@gmail.com>
2022-12-15 12:54:29 -0300

    marco-1-completo

---
+++ marco-1-completo/script.js
@@ -0,0 +1,11 @@
+function mostraValorCorreto1() {
+  const valorCorreto =
+    "Alternativa correta: O Senhor dos Anéis - Os Anéis de Poder";
+  alert(valorCorreto);
+}
+
+function mostraValorCorreto2() {
+  const valorCorreto =
+    "Alternativa correta: Running Up That Hill (A Deal With God) da Kate Bush";
+  alert(valorCorreto);
+}
