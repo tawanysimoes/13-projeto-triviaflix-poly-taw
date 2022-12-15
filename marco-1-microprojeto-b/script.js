df3468ee05271d9c57112e9a93dfe00cc15faa69
tawanysimoes <tawanyjor@gmail.com>
2022-12-15 12:50:22 -0300

    marco-1-microprojeto-b

---
+++ marco-1-microprojeto-b/script.js
@@ -0,0 +1,23 @@
+function resultadoHobbit() {
+  alert("O Hobbit");
+}
+
+function resultadoAneisPoder() {
+  alert("O Senhor dos Anéis: Os Anéis de Poder");
+}
+
+function resultadoCasaDragao() {
+  alert("A Casa do Dragão");
+}
+
+function resultadoKateBush() {
+  alert("Running Up That Hill (A Deal With God) da Kate Bush");
+}
+
+function resultadoCyndiLauper() {
+  alert("Time After Time da Cyndi Lauper");
+}
+
+function resultadoTheRunaways() {
+  alert("Dead end Justice do The Runaways");
+}
