8b9882f345e4bb3cdf1570a3f65411c4f36ca8c7
tawanysimoes <tawanyjor@gmail.com>
2022-12-15 12:52:27 -0300

    marco-1-microprojeto-c

---
+++ marco-1-microprojeto-c/script.js
@@ -0,0 +1,13 @@
+function mostraValor1() {
+  const triviaflix = document.querySelector(
+    'input[name="triviaflix1"]:checked'
+  ).value;
+  alert(triviaflix);
+}
+
+function mostraValor2() {
+  const triviaflix = document.querySelector(
+    'input[name="triviaflix2"]:checked'
+  ).value;
+  alert(triviaflix);
+}
